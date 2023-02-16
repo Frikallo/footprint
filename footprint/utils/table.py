8dd11c273ec2906e4467b76b79f5e575c422086d
Noah <88942100+Frikallo@users.noreply.github.com>
2023-02-16 11:18:07 -0800

    Update table.py

--- footprint/utils/table.py
+++ footprint/utils/table.py
@@ -1,5 +1,5 @@
-
 def buildTable(tabularData):
+    table = []
     types = []
     for _ in tabularData:
         types.append(_[0])
@@ -8,12 +8,13 @@ def buildTable(tabularData):
     for _ in tabularData:
         values.append(_[1])
     value = (len(max(values, key=len))+2)*"-"
-    print(f"+{type}+{value}+")
+    table.append(f"+{type}+{value}+")
     for _ in tabularData:
         spacing = (len(type)-len(_[0])-2)*" "
         trailingSpace = (len(value)-len(_[1])-2)*" "
-        print(f"| {_[0]}{spacing} | {_[1]}{trailingSpace} |")
-    print(f"+{type}+{value}+")
+        table.append(f"| {_[0]}{spacing} | {_[1]}{trailingSpace} |")
+    table.append(f"+{type}+{value}+")
+    return "\n".join(table)
 
 if __name__ == "__main__":
     tabularData = [
@@ -23,4 +24,4 @@ if __name__ == "__main__":
         ["SomeLongTypeNameData", "SomeValueData"],
         ["ShortName", "SomeLongValueData"],
     ]
-    buildTable(tabularData)
+    print(buildTable(tabularData))
